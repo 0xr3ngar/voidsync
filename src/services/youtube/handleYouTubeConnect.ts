6443d6303013e83177ad238c7c2032c164521898
0xr3ngar <bogdan.nikolov4@Outlook.com>
2025-10-12 02:14:36 +0200

    chore: init auth handlers

---
+++ src/services/youtube/handleYouTubeConnect.ts
@@ -0,0 +1,8 @@
+import type { Platform } from "@/models/Platform";
+
+export const handleYouTubeConnect = (
+    platform: Platform,
+    onConnect: (isConnected: boolean) => void,
+) => {
+    // TODO: Implement YouTube connection
+};

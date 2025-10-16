6e484f1c0816785969e30f93587b3f564011b66d
0xr3ngar <bogdan.nikolov4@Outlook.com>
2025-10-16 22:20:07 +0200

    feat: initialize global OAuth server on application start

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 #! /usr/bin/env bun
 import { render } from "ink";
 import { App } from "@/App.tsx";
+import { startGlobalOAuthServer } from "@/services/shared/oauthServer";
 
 process.stdout.write("\x1Bc");
+
+startGlobalOAuthServer();
 
 render(<App />, {
     exitOnCtrlC: true,
